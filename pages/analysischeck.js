84abc1a458f1e788347d3ce495a26b66fbfd5966
Hammad Yousaf <hammad.yousaf.1929@slashnext.net>
2025-04-22 15:21:59 +0500

    adding pipeline

--- pages/analysischeck.js
+++ pages/analysischeck.js
@@ -1,6 +1,6 @@
 // File: pages/api/analysisCheck.js
 
-import { pool } from '../../../lib/db';
+import { pool } from '../lib/db';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
